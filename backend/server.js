ab1f3ff3d30dbf8d9cf0ef62df7e0acc993316e7
Rushit Patel <rushitpatel163@gmail.com>
2024-04-03 21:31:46 +0530

    Done and dusted

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,36 @@
 const express = require('express')
 const dotenv = require('dotenv')
-const productRoutes = require('./routes/productRoutes')
-
-
+const cors = require('cors');
+const axios = require('axios')
 dotenv.config()
 const app = express();
 
 
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
-app.use('/categories', productRoutes);
+app.get('/categories/:categoryName/products', async (req, res) => {
+    try {
+        const { categoryName } = req.params;
+        const { companyName,minPrice, maxPrice } = req.query;
+        let apiUrl= `http://20.244.56.144/test/companies/${companyName}/categories/${categoryName}/products?top=${10}&minPriCe=${minPrice}&maxPrice=${maxPrice}`;;
+        
+        const response = await axios.get(apiUrl, {
+            headers: {
+              Authorization: {
+  token_type: "Bearer",
+  access_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzEyMTU5NjQ4LCJpYXQiOjE3MTIxNTkzNDgsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjFhMDU0NjAzLWY1MmItNDBhMC1iYTQ0LTcxZjQwOThmNjFlMSIsInN1YiI6InJoNzIzMkBzcm1pc3QuZWR1LmluIn0sImNvbXBhbnlOYW1lIjoiQ29kZXgiLCJjbGllbnRJRCI6IjFhMDU0NjAzLWY1MmItNDBhMC1iYTQ0LTcxZjQwOThmNjFlMSIsImNsaWVudFNlY3JldCI6IlljWHlDdHZuYkdLUlFHRUsiLCJvd25lck5hbWUiOiJSdXNoaXQiLCJvd25lckVtYWlsIjoicmg3MjMyQHNybWlzdC5lZHUuaW4iLCJyb2xsTm8iOiJSQTIxMTEwMDMwMTAzNDkifQ.syVOF6PySYogdYJN5GUjd9T01FryxdGUnCstuem5De8",
+  expires_in: 1712159648
+}
+            }
+          });
+        console.log(response.data)
+        res.json(response.data.products);
+    } catch (error) {
+        console.error('Error fetching products:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
 app.listen(process.env.PORT,()=>{
     console.log(process.env.PORT);
 })
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ function App() {
   const [selected2, setSelected2] = useState(null);
   const [minPrice,setMinPrice] = useState(0);
   const [maxPrice,setMaxPrice] = useState(0);
+  const [view,setView] = useState(false);
   function onChange(i) {
     setSelected((prev) => (i === prev ? null : i));
   }
@@ -22,7 +23,8 @@ function App() {
   function selectMax(k) {
     setMaxPrice((p) => p=k);
   } 
-  function handleSubmit(){
+  const  handleSubmit= async()=>{
+    setView(prev=>!prev)
     const retailerMap = {
       'Amazon': 'AMZ',
       'Flipcart': 'FLP',
@@ -30,10 +32,16 @@ function App() {
       'Myntra': 'MYN',
       'Ajio': 'AZO'
   }
-  const api = `http://20.244.56.144/test/companies/${retailerMap[options[selected]]}/categories/${options2[selected2]}/products?top=10&minPrice=${minPrice}&maxPrice=${maxPrice}`;
+  console.log(retailerMap[options[selected]])
   try{
-    const res = axios.get(api);
-    console.log(res)
+    const res = await axios.get(`http://localhost:5000/categories/${options2[selected2]}/products`, {
+      params: {
+          companyName: retailerMap[options[selected]],
+          minPrice : minPrice,
+          maxPrice : maxPrice
+      }
+  })
+  console.log(res)
   }catch(e){
     console.log(e);
   }
@@ -114,8 +122,9 @@ function App() {
           Products
         </h2>
       </div>
-  
+     {view &&
       <ul className="grid gap-4 mt-8 sm:grid-cols-2 lg:grid-cols-4">
+        
           <li>
           <a href={`/product/`} className="block overflow-hidden group">
             <img
@@ -140,9 +149,270 @@ function App() {
             </div>
           </a>
         </li>
-    
-        
+        <li>
+          <a href={`/product/`} className="block overflow-hidden group">
+            <img
+              src={laptop}
+              alt=""
+              className="h-[350px] w-full object-fill transition duration-500 group-hover:scale-105 sm:h-[450px]"
+            
+            />
+  
+            <div className="relative pt-3 bg-white">
+              <h3
+                className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
+              >
+                hello
+              </h3>
+  
+              <p className="mt-2">
+                <span className="sr-only"> Regular Price </span>
+  
+                <span className="tracking-wider text-gray-900"> 2234 </span>
+              </p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href={`/product/`} className="block overflow-hidden group">
+            <img
+              src={laptop}
+              alt=""
+              className="h-[350px] w-full object-fill transition duration-500 group-hover:scale-105 sm:h-[450px]"
+            
+            />
+  
+            <div className="relative pt-3 bg-white">
+              <h3
+                className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
+              >
+                hello
+              </h3>
+  
+              <p className="mt-2">
+                <span className="sr-only"> Regular Price </span>
+  
+                <span className="tracking-wider text-gray-900"> 2234 </span>
+              </p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href={`/product/`} className="block overflow-hidden group">
+            <img
+              src={laptop}
+              alt=""
+              className="h-[350px] w-full object-fill transition duration-500 group-hover:scale-105 sm:h-[450px]"
+            
+            />
+  
+            <div className="relative pt-3 bg-white">
+              <h3
+                className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
+              >
+                hello
+              </h3>
+  
+              <p className="mt-2">
+                <span className="sr-only"> Regular Price </span>
+  
+                <span className="tracking-wider text-gray-900"> 2234 </span>
+              </p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href={`/product/`} className="block overflow-hidden group">
+            <img
+              src={laptop}
+              alt=""
+              className="h-[350px] w-full object-fill transition duration-500 group-hover:scale-105 sm:h-[450px]"
+            
+            />
+  
+            <div className="relative pt-3 bg-white">
+              <h3
+                className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
+              >
+                hello
+              </h3>
+  
+              <p className="mt-2">
+                <span className="sr-only"> Regular Price </span>
+  
+                <span className="tracking-wider text-gray-900"> 2234 </span>
+              </p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href={`/product/`} className="block overflow-hidden group">
+            <img
+              src={laptop}
+              alt=""
+              className="h-[350px] w-full object-fill transition duration-500 group-hover:scale-105 sm:h-[450px]"
+            
+            />
+  
+            <div className="relative pt-3 bg-white">
+              <h3
+                className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
+              >
+                hello
+              </h3>
+  
+              <p className="mt-2">
+                <span className="sr-only"> Regular Price </span>
+  
+                <span className="tracking-wider text-gray-900"> 2234 </span>
+              </p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href={`/product/`} className="block overflow-hidden group">
+            <img
+              src={laptop}
+              alt=""
+              className="h-[350px] w-full object-fill transition duration-500 group-hover:scale-105 sm:h-[450px]"
+            
+            />
+  
+            <div className="relative pt-3 bg-white">
+              <h3
+                className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
+              >
+                hello
+              </h3>
+  
+              <p className="mt-2">
+                <span className="sr-only"> Regular Price </span>
+  
+                <span className="tracking-wider text-gray-900"> 2234 </span>
+              </p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href={`/product/`} className="block overflow-hidden group">
+            <img
+              src={laptop}
+              alt=""
+              className="h-[350px] w-full object-fill transition duration-500 group-hover:scale-105 sm:h-[450px]"
+            
+            />
+  
+            <div className="relative pt-3 bg-white">
+              <h3
+                className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
+              >
+                hello
+              </h3>
+  
+              <p className="mt-2">
+                <span className="sr-only"> Regular Price </span>
+  
+                <span className="tracking-wider text-gray-900"> 2234 </span>
+              </p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href={`/product/`} className="block overflow-hidden group">
+            <img
+              src={laptop}
+              alt=""
+              className="h-[350px] w-full object-fill transition duration-500 group-hover:scale-105 sm:h-[450px]"
+            
+            />
+  
+            <div className="relative pt-3 bg-white">
+              <h3
+                className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
+              >
+                hello
+              </h3>
+  
+              <p className="mt-2">
+                <span className="sr-only"> Regular Price </span>
+  
+                <span className="tracking-wider text-gray-900"> 2234 </span>
+              </p>
+            </div>
+          </a>
+        </li><li>
+          <a href={`/product/`} className="block overflow-hidden group">
+            <img
+              src={laptop}
+              alt=""
+              className="h-[350px] w-full object-fill transition duration-500 group-hover:scale-105 sm:h-[450px]"
+            
+            />
+  
+            <div className="relative pt-3 bg-white">
+              <h3
+                className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
+              >
+                hello
+              </h3>
+  
+              <p className="mt-2">
+                <span className="sr-only"> Regular Price </span>
+  
+                <span className="tracking-wider text-gray-900"> 2234 </span>
+              </p>
+            </div>
+          </a>
+        </li><li>
+          <a href={`/product/`} className="block overflow-hidden group">
+            <img
+              src={laptop}
+              alt=""
+              className="h-[350px] w-full object-fill transition duration-500 group-hover:scale-105 sm:h-[450px]"
+            
+            />
+  
+            <div className="relative pt-3 bg-white">
+              <h3
+                className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
+              >
+                hello
+              </h3>
+  
+              <p className="mt-2">
+                <span className="sr-only"> Regular Price </span>
+  
+                <span className="tracking-wider text-gray-900"> 2234 </span>
+              </p>
+            </div>
+          </a>
+        </li><li>
+          <a href={`/product/`} className="block overflow-hidden group">
+            <img
+              src={laptop}
+              alt=""
+              className="h-[350px] w-full object-fill transition duration-500 group-hover:scale-105 sm:h-[450px]"
+            
+            />
+  
+            <div className="relative pt-3 bg-white">
+              <h3
+                className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
+              >
+                hello
+              </h3>
+  
+              <p className="mt-2">
+                <span className="sr-only"> Regular Price </span>
+  
+                <span className="tracking-wider text-gray-900"> 2234 </span>
+              </p>
+            </div>
+          </a>
+        </li>
+      
       </ul>
+}  
     </div>
   </>
   );
